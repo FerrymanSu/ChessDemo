5a69ca60e0b2b185bf5afa4bf1027a7e31f0a0c1
sixu1234 <105592866+sixu1234@users.noreply.github.com>
2022-05-22 21:53:57 +0800

    Initial commit

--- src/view/ChessGameFrame.java
+++ src/view/ChessGameFrame.java
@@ -2,6 +2,7 @@ package view;
 
 import com.tedu.manager.MusicPlayer;
 import controller.GameController;
+import model.ChessColor;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,6 +27,7 @@ public class ChessGameFrame extends JFrame implements ActionListener {
     public MusicPlayer musicPlayer;
     JPanel panel;
     private JLabel hintLabel = new JLabel("Turn For WHITE");
+    private JLabel daoJiShi = new JLabel("TimeLeft: 60 s");
 
 
 
@@ -40,20 +42,6 @@ public class ChessGameFrame extends JFrame implements ActionListener {
     public String getBGM() {
         return BGM;
     }
-
-   /* public void setBGC(Color BGC) {
-        this.BGC = BGC;
-    }
-
-    public void setBack(String background) {
-        this.back = background;
-    }
-
-    public void setBGM(String BGM) {
-        this.BGM = BGM;
-    }*/
-
-
 
     public ChessGameFrame(String back, String BGM, Color BGC) {
         setTitle("2022 CS102A Project Demo"); //设置标题
@@ -81,30 +69,13 @@ public class ChessGameFrame extends JFrame implements ActionListener {
         panel = new JPanel();
         panel.setLayout(null);
 
-        JButton bt3 = createButton("Restart");
-        bt3.setActionCommand("3");
-        bt3.setLocation(800,150);
-        bt3.setSize(160,50);
-        bt3.addActionListener(this);
-
-        JButton bt6 = createButton("Playback");
-        bt6.setSize(160,50);
-        bt6.setActionCommand("6");
-        bt6.setLocation(800,450);
-        bt6.addActionListener(this);
-
-        JButton bt8 = createButton("Change BGM");
-        bt8.setSize(160,50);
-        bt8.setActionCommand("8");
-        bt8.setLocation(800, 650);
-        bt8.addActionListener(this);
 
 
-        panel.add(bt3);
-        panel.add(bt6);
-        panel.add(bt8);
+
+
         panel.add(jl);
-
+        addDaoJiShi();
+        addRestartButton();
         addChessboard();
         addHintLabel();
         addChangeBGI();
@@ -119,11 +90,23 @@ public class ChessGameFrame extends JFrame implements ActionListener {
 
 
     }
+    public void addDaoJiShi(){
+        this.daoJiShi.setFont(new Font("Castellar", Font.BOLD, 23));
+        this.daoJiShi.setSize(250, 60);
+        this.daoJiShi.setLocation(760,170);
+        this.daoJiShi.setForeground(new Color(246, 231, 215));
+        Time time = new Time(daoJiShi, chessboard);
+        time.start();
+        add(daoJiShi);
+    }
+
     public void addHintLabel(){
-        this.hintLabel.setFont(new Font("Castellar", Font.BOLD, 20));
+        this.hintLabel.setFont(new Font("Castellar", Font.BOLD, 21));
         this.hintLabel.setSize(250,60);
         this.hintLabel.setLocation(760,70);
         this.hintLabel.setForeground(new Color(246, 231, 215));
+        //hintLabel.setText(chessboard.getCurrentColor() == ChessColor.WHITE ? "Turn For WHITE": "Turn For BLACK");
+        //repaint();
         add(hintLabel);
     }
 
@@ -131,25 +114,26 @@ public class ChessGameFrame extends JFrame implements ActionListener {
         chessboard = new Chessboard(650,650);
         chessboard.setHintLabel(hintLabel);
         gameController = new GameController(chessboard);
-        chessboard.setLocation(getWidth()/20, getWidth()/20);
+        chessboard.setLocation(getWidth()/20, getHeight()/20);
         add(chessboard);
     }
+    public void addRestartButton(){
+        JButton bt3 = createButton("Restart");
+        bt3.setActionCommand("3");
+        bt3.setLocation(800,320);
+        bt3.setSize(160,50);
+        add(bt3);
 
-    private void addLabel() {
-        JLabel statusLabel = new JLabel("Sample label");
-        statusLabel.setLocation(HEIGHT, HEIGHT / 10);
-        statusLabel.setSize(200, 60);
-        statusLabel.setFont(new Font("Castellar", Font.BOLD, 12));
-        add(statusLabel);
+        bt3.addActionListener(e->{
+
+        });
     }
 
+
     private void addSaveButton() {
-        JButton button = new JButton("Save");
-        button.setBackground(new Color(246, 231, 215));
-        button.setLocation(800, 250);
-        button.setSize(160, 50);
-        button.setFont(new Font("Castellar", Font.BOLD, 12));
-        panel.add(button);
+        JButton button = createButton("Save");
+        button.setLocation(800, 420);
+        add(button);
 
         button.addActionListener(e -> {
             System.out.println("Click load");
@@ -158,12 +142,9 @@ public class ChessGameFrame extends JFrame implements ActionListener {
     }
 
     private void addLoadButton() {
-        JButton button = new JButton("Load");
-        button.setBackground(new Color(246, 231, 215));
-        button.setLocation(800, 350);
-        button.setSize(160, 50);
-        button.setFont(new Font("Castellar", Font.BOLD, 12));
-        panel.add(button);
+        JButton button = createButton("Load");
+        button.setLocation(800, 520);
+        add(button);
 
         button.addActionListener(e -> {
             gameController.showFileOpenDialog(this);
@@ -173,10 +154,11 @@ public class ChessGameFrame extends JFrame implements ActionListener {
     private void addBackButton() {
         JButton button = new JButton("StepBack");
         button.setBackground(new Color(246, 231, 215));
-        button.setLocation(320, 700);
+        button.setLocation(540, 700);
         button.setSize(160, 50);
-        button.setFont(new Font("Castellar", Font.BOLD, 12));
-        panel.add(button);
+        button.setForeground(new Color(138, 93, 47));
+        button.setFont(new Font("Castellar", Font.BOLD, 14));
+        add(button);
 
         button.addActionListener(e -> {
             gameController.stepBack();
@@ -188,8 +170,9 @@ public class ChessGameFrame extends JFrame implements ActionListener {
         button.setBackground(new Color(246, 231, 215));
         button.setLocation(50, 700);
         button.setSize(160, 50);
-        button.setFont(new Font("Castellar", Font.BOLD, 12));
-        panel.add(button);
+        button.setForeground(new Color(138, 93, 47));
+        button.setFont(new Font("Castellar", Font.BOLD, 14));
+        add(button);
 
         button.addActionListener(e -> {
             gameController.stepForward();
@@ -212,35 +195,18 @@ public class ChessGameFrame extends JFrame implements ActionListener {
     public JButton createButton(String text){
         JButton btn = new JButton(text);
         btn.setSize(new Dimension(160, 50));
-        Font font = new Font("Castellar", Font.BOLD, 10);
+        Font font = new Font("Castellar", Font.BOLD, 14);
+        btn.setForeground(new Color(138, 93, 47));
         btn.setFont(font);
         btn.setBackground(new Color(246, 231, 215));
         return btn;
     }
     public void addChangeBGI(){
         JButton button = createButton("Change BGI");
-        button.setLocation(800, 550);
+        button.setLocation(800, 620);
         button.setSize(160,50);
-        panel.add(button);
+        add(button);
         button.addActionListener(e -> {
-            /*JFileChooser chooser = new JFileChooser();
-            chooser.setMultiSelectionEnabled(false);
-
-            int value = chooser.showOpenDialog(button);
-            if(value == JFileChooser.APPROVE_OPTION){
-                panel.remove(jl);
-                repaint();
-
-                String path = chooser.getSelectedFile().getAbsolutePath();
-
-                ImageIcon image= new ImageIcon(path);
-                Image image1 = image.getImage();
-                Image image2 = image1.getScaledInstance(getWidth(),getHeight(), Image.SCALE_FAST);
-                ImageIcon trueBack = new ImageIcon(image2);
-                jl = new JLabel(trueBack);
-                jl.setBounds(0,0, getWidth(), getHeight());
-                panel.add(jl);
-            }*/
             JFrame frame = new JFrame("Change BGI");
             frame.setLocation(getWidth()/2, getHeight()/2);
             frame.setSize(300,400);
@@ -257,7 +223,7 @@ public class ChessGameFrame extends JFrame implements ActionListener {
             jb.setBounds(0,0, frame.getWidth(), frame.getHeight());
 
             JLabel jl1 = new JLabel("切换背景");
-            jl1.setLocation(100,100);
+            jl1.setLocation(100,50);
             jl1.setSize(190,50);
             jl1.setFont(new Font("造字工房米萌体", Font.BOLD, 30));
             jl1.setForeground(new Color(108, 51, 4));
@@ -265,6 +231,7 @@ public class ChessGameFrame extends JFrame implements ActionListener {
 
             JButton bt1 = createButton("钢铁烈风");
             bt1.setFont(new Font("造字工房米萌体", Font.BOLD, 15));
+            bt1.setForeground(new Color(108,51,4));
             bt1.setBackground(new Color(207,109,60));
             bt1.addActionListener(e1 -> {
                 panel.remove(jl);
@@ -278,12 +245,15 @@ public class ChessGameFrame extends JFrame implements ActionListener {
                 jl.setBounds(0,0, getWidth(), getHeight());
                 panel.add(jl);
 
+                chessboard.change(new Color(232,232,232), new Color(102,102,102));
+
                 frame.setVisible(false);
 
             });
-            bt1.setLocation(70, 170);
+            bt1.setLocation(70, 120);
 
             JButton bt2 = createButton("碧蓝天空");
+            bt2.setForeground(new Color(108,51,4));
             bt2.setFont(new Font("造字工房米萌体", Font.BOLD, 15));
             bt2.setBackground(new Color(207,109,60));
             bt2.addActionListener(e1 -> {
@@ -298,15 +268,40 @@ public class ChessGameFrame extends JFrame implements ActionListener {
                 jl.setBounds(0,0, getWidth(), getHeight());
                 panel.add(jl);
 
+                chessboard.change(new Color(255,255,255), new Color(32,96,184));
+
                 frame.setVisible(false);
 
             });
-            bt2.setLocation(70, 240);
+            bt2.setLocation(70, 190);
+
+            JButton bt3 = createButton("原背景");
+            bt3.setForeground(new Color(108,51,4));
+            bt3.setFont(new Font("造字工房米萌体", Font.BOLD, 15));
+            bt3.setBackground(new Color(207,109,60));
+            bt3.addActionListener(e1->{
+                panel.remove(jl);
+                repaint();
+
+                ImageIcon picture= new ImageIcon("images/bac1.jpg");
+                Image picture1 = picture.getImage();
+                Image picture2 = picture1.getScaledInstance(getWidth(),getHeight(), Image.SCALE_FAST);
+                ImageIcon trueP = new ImageIcon(picture2);
+                jl = new JLabel(trueP);
+                jl.setBounds(0,0, getWidth(), getHeight());
+                panel.add(jl);
+
+                chessboard.change(new Color(103,67,49), new Color(231,203,167));
+
+                frame.setVisible(false);
+            });
+            bt3.setLocation(70,270);
 
 
             panel1.add(jb);
-            panel1.add(bt1);
-            panel1.add(bt2);
+            frame.add(bt1);
+            frame.add(bt2);
+            frame.add(bt3);
             frame.add(jl1);
             frame.add(panel1);
             frame.setVisible(true);
--- src/view/Time.java
+++ src/view/Time.java
@@ -1,17 +1,40 @@
 package view;
 
 import javax.swing.*;
-import java.util.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 public class Time {
     private JLabel DaoJiShi;
     private int leftTime = 60;
-    int sec;
     Timer countDown;
+    private Chessboard chessboard;
 
 
-    public Time(JLabel daoJiShi){
+    public Time(JLabel daoJiShi, Chessboard chessboard){
         this.DaoJiShi = daoJiShi;
-
+        this.chessboard = chessboard;
+        countDown = new Timer(1000, new countDownListener());
+    }
+    public void reset(){
+        leftTime = 60;
+    }
+    public void start(){
+        countDown.start();
+    }
+    class countDownListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(leftTime > 0){
+                DaoJiShi.setText("TimeLeft: " + leftTime + " s");
+                leftTime--;
+            }else {
+                DaoJiShi.setText("Time's Up!");
+                reset();
+                start();
+                chessboard.swapColor();
+            }
+        }
     }
 }
